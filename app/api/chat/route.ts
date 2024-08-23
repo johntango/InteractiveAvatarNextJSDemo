85708dc18b15b21a3f4aa2a96423a217df29b5a3
John Williams <jrw+3@mit.edu>
2024-08-23 15:40:51 +0000

    openai agent added

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,24 @@
-import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
-
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
-
 export async function POST(req: Request) {
-  const { messages } = await req.json();
-
-  const result = await streamText({
-    model: openai("gpt-4-turbo"),
-    messages,
-  });
-
-  return result.toAIStreamResponse();
-}
+    const { messages } = await req.json();
+    try {
+        const url = "https://studious-space-xylophone-xvw65v5xp94364p5-3000.app.github.dev/openai/agent"
+      const res = await fetch(
+        url,
+        {
+          method: "POST",
+          prompt: messages
+        }
+      );
+      const data = await res.json();
+  
+      return new Response(data.text, {
+        status: 200,
+      });
+    } catch (error) {
+      console.error("Error retrieving access token:", error);
+  
+      return new Response("Failed to retrieve access token", {
+        status: 500,
+      });
+    }
+  }
